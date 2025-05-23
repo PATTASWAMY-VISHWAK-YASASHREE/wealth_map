5431706aa7059a612177aace019e4c1613a36b7d
vara-prasad-07 <varaprasad2684@gmail.com>
2025-05-23 10:17:10 +0530

    bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
-import gmaps from '../components/gmaps.vue'
+
 import MapView from '../components/MapView.vue'
 
 const routes = [
   { path: '/', component: MapView },
   { path: '/hello', component: HelloWorld },
-  {path: '/map', component: MapView },
-  {path: '/maps', component: gmaps },
+  {path: '/map', component: MapView }
 ]
 
 const router = createRouter({
